d775bf986b5d10b1f2036bd02b14a2d089b9278b
Chris <cec213@scarletmail.rutgers.edu>
2020-11-26 18:01:21 -0500

    working deployment

---
+++ routes/apiRoutes.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+let data = JSON.parse(fs.readFileSync("./db/db.json"));
+
+module.exports = function(app) {
+    //see notes
+    app.get("/api/notes", function(req, res) {
+       res.json(data);
+    });
+    //creates note
+    app.post("/api/notes", function(req, res) {
+
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+        res.json(data);    
+    });
+    //deletes note
+    app.delete("/api/notes/:id", function(req, res) {
+        let noteId = req.params.id;
+        let newId = 0;
+        data = data.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        for (currentNote of data) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        res.json(data);
+    }); 
+}
+
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require("express");
-
 var app = express();
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
-
+app.use(express.static("public"));
 
 require("./routes/htmlRoutes.js")(app);
-// require("./routes/apiRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
